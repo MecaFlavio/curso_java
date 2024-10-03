8ae9dc359e591236bbe179c546459bea8e5896d5
Flavio A. Pereira <flavio.metodista@hotmail.com>
2024-10-03 04:56:41 -0300

    Adicionado anotações sobre Manipulaçãp de data e hora, enumeração e composição

---
+++ tipos_enumerados/src/tipos_enumerados/Programa.java
@@ -0,0 +1,25 @@
+package tipos_enumerados;
+
+import java.util.Date;
+
+import entities.Order;
+import etities.enums.OrderStatus;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Order order = new Order (1080, new Date(), OrderStatus.PENDING_PAYMENT);
+		
+		System.out.println(order);
+
+		// Converção de String para enum
+		
+		OrderStatus os1 = OrderStatus.DELIVERED;
+		OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+		
+		System.out.println(os1);
+		System.out.println(os2);
+	}
+
+}
